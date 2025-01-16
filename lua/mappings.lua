4f45a949dd9eab9e72f44d47d4091224e9f97764
livghit <liviu.g01@gmail.com>
2025-01-16 21:19:30 +0100

    ⛏️ wip

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree focus window" })
+map("n", "<leader>at", "<cmd>TSEnable highlight indent incremental_selection<CR>", { desc = "enable treesitter" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
